2c13d008087883ec85dc79d6946b63a739509ba0
Muhammad Nisar <muhammadnisarbsse7@gmail.com>
2025-03-06 23:37:29 +0500

    Server is ready

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,26 @@
-console.log('Task Manager App In Running');
+const express =require ("express");
+const app =express();
+const tasks =require("./routes/tasks")
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello",(req,res)=>{
+    res.send("Task Manager App is Ready  to Fly")
+})
+
+app.use("/api/v1/tasks",tasks)
+
+
+
+
+
+
+
+
+
+const port =3000
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
