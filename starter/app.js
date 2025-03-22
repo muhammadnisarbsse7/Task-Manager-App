78c4950f633b775be9deea721a2789316a23c3f9
Muhammad Nisar <muhammadnisarbsse7@gmail.com>
2025-03-22 16:04:22 +0500

    Task updated

--- starter/app.js
+++ starter/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // routes
 app.get("/hello",(req,res)=>{
-    res.send("Task Manager App is Ready  to Fly")
+    res.send("Task Manager App is Ready")
 })
 
 app.use("/api/v1/tasks",tasks)
